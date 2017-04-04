fe7b25ca580ab33d6e18b3e9be409aa4fdeef021
DESKTOP-RNUM96T\William Rubiano <william.rubiano@restituciondetierras.gov.co>
2017-04-04 17:41:58 -0500

    Se realizo cambio en la clase de persona dto

--- inventario/src/app/clases/persona-dto.ts
+++ inventario/src/app/clases/persona-dto.ts
@@ -3,6 +3,7 @@ export class PersonaDTO {
     id: number;
     title: string = '';
     complete: boolean = false;
+    apellido: string = '';
 
     constructor(values: Object = {}) {
         Object.assign(this, values);
